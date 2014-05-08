7e89b73d1207baab5aa07d4d16047f5e7500e809
Valery Nemychnikova <sooobus@gmail.com>
2014-05-08 19:04:33 +0400

    Deleted GitHubEvent class and dependencies. Started implementing Parser

--- Parser.py
+++ Parser.py
@@ -6,6 +6,8 @@ Contains Parser class to download GitHub archives.
 
 
 from yajl import *
+from collections import deque
+
 
 #TODO: make parser using yajl
 
@@ -15,35 +17,52 @@ class Parser(object):
     def __init__(self, json_file):
         self.json_file = json_file
         self.handler = ContentHandler()
-    def get_event(self):
-        """Parse files in data folder and get Events. If returns None => needs to download new files"""
+        self.line = deque() #popleft(), append()
 
-        return event
-    
+    def get_event(self):
+        """Returns event from line. If returns None => needs to download new files"""
+        if self.line:
+            event = self.line.popleft()
+            return event
+        else:
+            return None
+
 class ContentHandler(YajlContentHandler):
     def __init__(self):
-        self.out = sys.stdout
-    def yajl_null(self, ctx):
+        self.bracket_stack = deque() #append(), pop()
+        self.event = None
+        self.cur_field = None
+
+    def parse_start(self):
+        self.event = {}
+
+    def yajl_null(self, ctx): #can be a dict data
         self.out.write("null\n" )
-    def yajl_boolean(self, ctx, boolVal):
+
+    def yajl_boolean(self, ctx, boolVal): #can be a dic data
         self.out.write("bool: %s\n" %('true' if boolVal else 'false'))
-    def yajl_integer(self, ctx, integerVal):
-        self.out.write("integer: %s\n" %integerVal)
-    def yajl_double(self, ctx, doubleVal):
-        self.out.write("double: %s\n" %doubleVal)
-    def yajl_number(self, ctx, stringNum):
-        ''' Since this is defined both integer and double callbacks are useless '''
+
+    def yajl_number(self, ctx, stringNum): #can be a dict data
+        #TODO: develop...
         num = float(stringNum) if '.' in stringNum else int(stringNum)
         self.out.write("number: %s\n" %num)
+
     def yajl_string(self, ctx, stringVal):
+        #TODO: develop...
         self.out.write("string: '%s'\n" %stringVal)
+
     def yajl_start_map(self, ctx):
-        self.out.write("map open '{'\n")
+        self.bracket_stack.append('{')
+
     def yajl_map_key(self, ctx, stringVal):
-        self.out.write("key: '%s'\n" %stringVal)
+        self.cur_field = stringVal
+
     def yajl_end_map(self, ctx):
-        self.out.write("map close '}'\n")
+        self.bracket_stack.pop()
+        self.cur_field = None
+
     def yajl_start_array(self, ctx):
-        self.out.write("array open '['\n")
+        self.bracket_stack.append('[')
+
     def yajl_end_array(self, ctx):
-        self.out.write("array close ']'\n")
+        self.bracket_stack.pop()
