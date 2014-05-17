9384aa7b019afb52f60eaad6b380b5d63507d4db
Andrey Kalendarov <zapretzone@gmail.com>
2014-05-17 15:48:04 +0400

    Reviewed, documented.

--- DataSaver.py
+++ DataSaver.py
@@ -2,12 +2,11 @@
 """ DataSaver.py """
 
 from os.path import join
-
 from FileSystemWorker import create_or_check_path, file_open
 
 
 class DataSaver(object):
-    """ """
+    """Methods of saving info about watchers."""
 
     def __init__(self):
         self.data_dir = "pairs/"
@@ -17,5 +16,8 @@ class DataSaver(object):
         create_or_check_path(self.data_file)
 
     def process_data(self, data):
+        """Saving pair user-repository in file.
+        :param data: processed event dictionary
+        """
         data_file = file_open(self.data_file, "a")
         data_file.write(data["actor"] + " " + data["url"] + "\n")
